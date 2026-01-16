5a97862f12aaa2e662ebfc39ad4998e36c14ca5b
Himanshu raj <kinghimanshurajyadav11@gmail.com>
2026-01-16 11:03:56 -0400

    commit to fix some error

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
     typescript: {
         //  Same for TS, better to ignore for MVP delivery if types are mostly fine but minor mismatches
         ignoreBuildErrors: true,
